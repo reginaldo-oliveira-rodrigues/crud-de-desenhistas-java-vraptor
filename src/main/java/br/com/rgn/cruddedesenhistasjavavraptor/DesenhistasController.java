a3ca0f0475f0c790a02cd883c3e0fecc439b2770
reginaldo-oliveira-rodrigues <regisolliver1@gmail.com>
2013-12-16 18:31:09 -0200

    Criando primeira classe controller a ser usada pelo VRaptor

---
+++ src/main/java/br/com/rgn/cruddedesenhistasjavavraptor/DesenhistasController.java
@@ -0,0 +1,10 @@
+package br.com.rgn.cruddedesenhistasjavavraptor;
+
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class DesenhistasController {
+	public void cadastro(){
+        System.out.println("Cadastro de desenhistas em desenvolvimento!");
+    }
+}
